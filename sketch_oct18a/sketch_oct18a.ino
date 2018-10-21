47d890b727a10fff84ae9f213b41218fd04d82b0
MarkNambatac <nambatacmarkangelo@gmail.com>
2018-10-21 23:42:08 +0800

    Version 1.0 of the Prototype

---
+++ sketch_oct18a/sketch_oct18a.ino
@@ -0,0 +1,177 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+//Pins Connected
+static const int RXPin = 3, TXPin = 2;
+static const uint32_t GPSBaud = 9600;
+const int button = 4;
+int warningLed = 5;
+int greenLed = 6;
+
+
+//Pulse Sensor
+int sensor_pin = 0;                
+int led_pin = 13;                  
+volatile int heart_rate;         
+volatile int analog_data;              
+volatile int time_between_beats = 600;            
+volatile boolean pulse_signal = false;    
+volatile int beat[10];        
+volatile int peak_value = 512;          
+volatile int trough_value = 512;        
+volatile int thresh = 525;              
+volatile int amplitude = 100;                 
+volatile boolean first_heartpulse = true;      
+volatile boolean second_heartpulse = false;    
+volatile unsigned long samplecounter = 0;   
+volatile unsigned long lastBeatTime = 0;
+
+
+
+TinyGPSPlus gps;
+SoftwareSerial ss(RXPin, TXPin);
+
+void setup()
+{
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+  pinMode(button, INPUT);
+  pinMode(warningLed , OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  interruptSetup();   
+}
+
+void loop()
+{
+  if (button == HIGH) {
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayInfo();
+  }
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print("\r");
+    delay(1000);
+    Serial.print("AT+CMGF=1\r");
+    delay(1000);
+    Serial.print("AT+CMGS=\"09058382987\"\r");
+    delay(1000);
+    Serial.print("www.google.com.ph/maps/place/");
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+    Serial.print("\r");
+    delay(1000);
+    Serial.println((char)26);
+    delay(1000);
+    Serial.write(0x1A);
+    Serial.write(0x0D);
+    Serial.write(0x0A);
+    delay(1000);  
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+  Serial.println();
+}
+
+
+
+void interruptSetup()
+{    
+  TCCR2A = 0x02;  // This will disable the PWM on pin 3 and 11
+  OCR2A = 0X7C;   // This will set the top of count to 124 for the 500Hz sample rate
+  TCCR2B = 0x06;  // DON'T FORCE COMPARE, 256 PRESCALER
+  TIMSK2 = 0x02;  // This will enable interrupt on match between OCR2A and Timer
+  sei();          // This will make sure that the global interrupts are enable
+}
+
+ISR(TIMER2_COMPA_vect)
+{ 
+  cli();                                     
+  analog_data = analogRead(sensor_pin);            
+  samplecounter += 2;                        
+  int N = samplecounter - lastBeatTime;      
+  if(analog_data < thresh && N > (time_between_beats/5)*3)
+    {     
+      if (analog_data < trough_value){                       
+        trough_value = analog_data;
+      }
+    }
+  if(analog_data > thresh && analog_data > peak_value)
+    {        
+      peak_value = analog_data;
+    }                          
+
+   if (N > 250)
+  {                            
+    if ( (analog_data > thresh) && (pulse_signal == false) && (N > (time_between_beats/5)*3) )
+      {       
+        pulse_signal = true;          
+        digitalWrite(led_pin,HIGH);
+        time_between_beats = samplecounter - lastBeatTime;
+        lastBeatTime = samplecounter;     
+
+       if(second_heartpulse)
+        {                        
+          second_heartpulse = false;   
+          for(int i=0; i<=9; i++)    
+          {            
+            beat[i] = time_between_beats; //Filling the array with the heart beat values                    
+          }
+        }
+
+        if(first_heartpulse)
+        {                        
+          first_heartpulse = false;
+          second_heartpulse = true;
+          sei();            
+          return;           
+        }  
+
+      word runningTotal = 0;  
+      for(int i=0; i<=8; i++)
+        {               
+          beat[i] = beat[i+1];
+          runningTotal += beat[i];
+        }
+      beat[9] = time_between_beats;             
+      runningTotal += beat[9];   
+      runningTotal /= 10;        
+      heart_rate = 60000/runningTotal;
+    }                      
+  }
+
+  if (analog_data < thresh && pulse_signal == true)
+    {  
+      digitalWrite(led_pin,LOW); 
+      pulse_signal = false;             
+      amplitude = peak_value - trough_value;
+      thresh = amplitude/2 + trough_value; 
+      peak_value = thresh;           
+      trough_value = thresh;
+    }
+
+  if (N > 2500)
+    {                          
+      thresh = 512;                     
+      peak_value = 512;                 
+      trough_value = 512;               
+      lastBeatTime = samplecounter;     
+      first_heartpulse = true;                 
+      second_heartpulse = false;               
+    }
+  sei();                                
+
+}
